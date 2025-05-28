19e22818de2df9463ee3004d9a017048130190d8
lam1704 <lamnthe180410@fpt.edu.vn>
2025-05-28 19:30:12 +0700

    Add VietQR Service

---
+++ CleanMate_Main.Server/Models/DTO/VietQR/QRGenerateModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CleanMate_Main.Server.Models.DTO.VietQR
+{
+    public class QRGenerateModel
+    {
+        public string AccountNo { get; set; } = null!;
+        public string AccountName { get; set; } = null!;
+        public int AcqId { get; set; }
+        public int Amount { get; set; }
+        public string? AddInfo { get; set; }
+        public string Template { get; set; } = "print";
+    }
+}

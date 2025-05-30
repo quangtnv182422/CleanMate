8140d06558350d0077dbd05245a8b03bd085f37a
dumspice <dzunghoang04@gmail.com>
2025-05-30 08:58:05 +0700

    feat: create order history and order history details

---
+++ cleanmate_main.client/src/components/OrderHistorySection/OrderHistorySection.jsx
@@ -0,0 +1,221 @@
+﻿import { useState } from 'react';
+import {
+    Box,
+    Grid,
+    Typography,
+    Card,
+    CardContent,
+    Pagination,
+    Button,
+    Modal,
+} from '@mui/material';
+import { style } from './style';
+import OrderDetails from '../../main-component/OrderDetails/OrderDetails';
+
+const mockOrders = Array(15).fill(null).map((_, index) => ({
+    id: `#362689FG-${index + 1}`,
+    service: 'Dọn dẹp theo giờ',
+    startTime: '10',
+    date: '20/5/2025',
+    price: 200000,
+    duration: 2,
+    note: 'Nhà có mèo, dọn kỹ gầm bàn, gầm giường, và các góc khuất khác. Không cần dọn bếp vì đã có người dọn.',
+    name: 'Hoàng Tiến Dũng',
+    address: 'Số nhà 30, Nguyễn Sơn, Bồ Đề, Long Biên, Hà Nội',
+    payment: 'Tiền mặt',
+    employee: 1,
+    status:
+        index % 4 === 0 ? 'canceled' :
+            index % 2 === 1 ? 'active' :
+                'completed',
+}));
+
+const ORDERS_PER_PAGE = 6;
+
+const colorMap = {
+    total: {
+        background: '#e3f2fd',
+        border: '#e3f2fd',
+        color: '#1976D2',
+    },
+    active: {
+        background: '#FBC11B',
+        border: '#FBC11B',
+        blurBackground: '#FFF8E1',
+        color: '#fff',
+    },
+    completed: {
+        background: '#5CBB52',
+        border: '#5CBB52',
+        blurBackground: '#E8F5E9',
+        color: '#fff',
+    },
+    canceled: {
+        background: '#E4293D',
+        border: '#E4293D',
+        blurBackground: '#FFEBEE',
+        color: '#fff',
+    },
+};
+
+const OrderHistorySection = () => {
+    const [selectedTab, setSelectedTab] = useState('total');
+    const [page, setPage] = useState(1);
+    const [open, setOpen] = useState(false);
+    const [selectedOrder, setSelectedOrder] = useState(null);
+
+    const handleOpenOrderDetails = (order) => {
+        setOpen(true);
+        setSelectedOrder(order);
+    };
+    const handleClose = () => setOpen(false);
+
+    const stats = {
+        active: mockOrders.filter((o) => o.status === 'active').length,
+        completed: mockOrders.filter((o) => o.status === 'completed').length,
+        canceled: mockOrders.filter((o) => o.status === 'canceled').length,
+    };
+    stats.total = mockOrders.length;
+
+    const filteredOrders = mockOrders.filter(order =>
+        selectedTab === 'total' ? true : order.status === selectedTab
+    );
+
+    const totalPages = Math.ceil(filteredOrders.length / ORDERS_PER_PAGE);
+    const displayedOrders = filteredOrders.slice(
+        (page - 1) * ORDERS_PER_PAGE,
+        page * ORDERS_PER_PAGE
+    );
+
+    const handleSelect = (type) => {
+        setSelectedTab(type);
+        setPage(1);
+    };
+
+    const formatPrice = (price) => {
+        return price.toLocaleString('vi-VN', {
+            style: 'currency',
+            currency: 'VND',
+        })
+    }
+
+    return (
+        <Box sx={style.orderHistorySection}>
+            <Box className="container" sx={style.container}>
+                {/* Tabs */}
+                <Grid container spacing={2} sx={{ mb: 4 }}>
+                    {['total', 'active', 'completed', 'canceled'].map((type) => {
+                        const selected = selectedTab === type;
+                        const colors = colorMap[type];
+                        return (
+                            <Grid item xs={12} sm={3} key={type}>
+                                <Card
+                                    onClick={() => handleSelect(type)}
+                                    sx={{
+                                        ...style.tabCard,
+                                        borderColor: colors.border,
+                                        backgroundColor: colors.background,
+                                        filter: selected ? 'brightness(0.8)' : 'brightness(1)',
+                                        color: colors.color,
+                                        '&:hover': {
+                                            filter: 'brightness(0.8)',
+                                        },
+                                    }}
+                                >
+                                    <CardContent sx={{
+                                        '@media (min-width: 600px)': {
+                                            p: 1,
+                                        }
+                                    }}>
+                                        <Typography
+                                            variant="h6"
+                                            sx={{
+                                                color: 'inherit',
+                                                mb: 1,
+                                                '@media (max-width: 600px)': {
+                                                    fontSize: '12px',
+                                                },
+                                            }}>
+                                            {type === 'total'
+                                                ? 'Tổng số đơn'
+                                                : type === 'active'
+                                                    ? 'Chờ xác nhận'
+                                                    : type === 'completed'
+                                                        ? 'Đơn hoàn thành'
+                                                        : 'Đơn đã hủy'}
+                                        </Typography>
+                                        <Typography variant="h4" fontWeight={600} sx={{ color: 'inherit' }}>
+                                            {stats[type]}
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        );
+                    })}
+                </Grid>
+
+                {/* Stats Summary */}
+                <Box sx={{ mt: 4, mb: 4, textAlign: 'center' }}>
+                    <Typography variant="body1" color="textSecondary">
+                        Tổng: {stats.total} | Đang xử lý: {stats.active} | Đã hoàn thành: {stats.completed} | Đã hủy: {stats.canceled}
+                    </Typography>
+                </Box>
+
+                {/* List of Orders */}
+                <Grid container spacing={2}>
+                    {displayedOrders.map((order, idx) => (
+                        <Grid item xs={12} sm={6} md={4} key={idx}>
+                            <Card sx={style.orderCard} onClick={() => handleOpenOrderDetails(order)}>
+                                <CardContent>
+                                    <Typography variant="body2" sx={{ mb: 1, color: 'gray' }}>Bắt đầu lúc {order.startTime} giờ ngày {order.date}</Typography>
+                                    <Typography sx={{ mt: 2, fontWeight: 500 }}>{order.name}</Typography>
+                                    <Typography variant="subtitle2" color="textSecondary">{order.address}</Typography>
+                                    <Box sx={style.priceSection}>
+                                        <Typography variant="h6" sx={{ color: '#1976D2' }}>Số tiền: {formatPrice(order.price)}</Typography>
+                                        <Typography
+                                            variant="subtitle2"
+                                            sx={{
+                                                ...style.status,
+                                                color: order.status === 'active' ? '#FBC11B' : order.status === 'completed' ? '#5CBB52' : '#E4293D',
+                                                backgroundColor: colorMap[order.status].blurBackground,
+                                                borderColor: colorMap[order.status].border,
+                                            }}
+                                        >
+                                            {order.status === "active"
+                                                ? "Chờ xác nhận"
+                                                : order.status === "completed"
+                                                    ? "Hoàn thành"
+                                                    : "Đã hủy"}
+                                        </Typography>
+                                    </Box>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+
+                {open && (
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                        disableAutoFocus
+                    >
+                        <OrderDetails selectedOrder={selectedOrder} />
+                    </Modal>
+                )}
+
+                {/* Pagination */}
+                <Box sx={{ mt: 4, display: 'flex', justifyContent: 'center' }}>
+                    <Pagination
+                        count={totalPages}
+                        page={page}
+                        onChange={(e, val) => setPage(val)}
+                        color="primary"
+                    />
+                </Box>
+            </Box>
+        </Box>
+    );
+};
+
+export default OrderHistorySection;
---
+++ cleanmate_main.client/src/main-component/OrderDetails/OrderDetails.jsx
@@ -0,0 +1,99 @@
+﻿import { useState } from 'react';
+import {
+    Box,
+    Typography,
+} from '@mui/material';
+import PendingIcon from '@mui/icons-material/Pending';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import { styles } from './style';
+const OrderDetails = ({ selectedOrder }) => {
+
+    const formatPrice = (price) => {
+        return price.toLocaleString("vi-VN", {
+            style: 'currency',
+            currency: 'VND',
+        })
+    }
+    return (
+        <Box sx={styles.modal}>
+            <Typography variant="h4" sx={styles.orderDetailTitle}>{selectedOrder.service}</Typography>
+
+            {/*Status*/}
+            <Box sx={styles.statusContainer}>
+                {
+                    selectedOrder.status === "active"
+                        ? (<PendingIcon sx={styles.iconLarge} color="warning" />)
+                        : selectedOrder.status === "completed"
+                            ? (<CheckCircleIcon sx={styles.iconLarge} color="success" />)
+                            : (<CancelIcon sx={styles.iconLarge} color="error" />)
+                }
+                <Typography sx={styles.status}>
+                    {selectedOrder.status === "active"
+                        ? "Chờ xác nhận"
+                        : selectedOrder.status === "completed"
+                            ? "Hoàn thành"
+                            : "Đã hủy"}
+                </Typography>
+            </Box>
+
+            {/*Order information*/}
+            <Box sx={styles.informationTitle}>
+                <AssignmentIcon size="small" sx={styles.subtitleIcon} />
+                <Typography variant="h6" sx={styles.subtitle}>Thông tin ca làm</Typography>
+            </Box>
+            <Box sx={styles.informationContent}>
+                <Box sx={{ mb: 1 }}>
+                    <Typography sx={styles.contentTitle}>Ngày làm việc</Typography>
+                    <Typography sx={styles.content}>Bắt đầu lúc {selectedOrder.startTime} giờ ngày {selectedOrder.date}</Typography>
+                </Box>
+                <Box sx={{ mb: 1 }}>
+                    <Typography sx={styles.contentTitle}>Địa chỉ</Typography>
+                    <Typography sx={styles.content}>{selectedOrder.address}</Typography>
+                </Box>
+
+                <Box sx={{ mb: 1 }}>
+                    <Typography sx={styles.contentTitle}>Nhân viên</Typography>
+                    <Typography sx={styles.content}>{selectedOrder.employee} nhân viên làm trong {selectedOrder.duration} giờ</Typography>
+                </Box>
+
+                <Box sx={{ mb: 1 }}>
+                    <Typography sx={styles.contentTitle}>Ghi chú</Typography>
+                    <Typography sx={styles.content}>{selectedOrder.note ? selectedOrder.note : "Không có ghi chú"}</Typography>
+                </Box>
+            </Box>
+
+            {/*Booking*/}
+            <Box sx={styles.informationTitle}>
+                <LibraryBooksIcon size="small" sx={styles.subtitleIcon} />
+                <Typography variant="h6" sx={styles.subtitle}>Đơn hàng</Typography>
+            </Box>
+
+            <Box sx={styles.informationContent}>
+                <Box sx={styles.bookingContent}>
+                    <Typography sx={styles.bookingTitle}>Số giờ/buổi</Typography>
+                    <Typography sx={styles.content}>{selectedOrder.duration}h/buổi</Typography>
+                </Box>
+
+                <Box sx={styles.bookingContent}>
+                    <Typography sx={styles.bookingTitle}>Hình thức thanh toán</Typography>
+                    <Typography sx={styles.content}>{selectedOrder.payment}</Typography>
+                </Box>
+
+                <Box sx={styles.bookingContent}>
+                    <Typography sx={styles.bookingTitle}>Tiền ca làm</Typography>
+                    <Typography sx={styles.content}>{formatPrice(selectedOrder.price)}</Typography>
+                </Box>
+
+                <Box sx={styles.bookingContent}>
+                    <Typography sx={styles.bookingTitle}>Tổng tiền</Typography>
+                    <Typography sx={styles.content}>{formatPrice(selectedOrder.price)}</Typography>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+export default OrderDetails;
